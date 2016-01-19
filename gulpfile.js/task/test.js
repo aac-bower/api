a4d70ebd71be56ed6c7f48ea8297ddc4c63c9d6a
skuijers <stefan@arteries.hu>
2016-01-19 16:44:00 +0100

    closes #1 | params support baseUrl

--- gulpfile.js/task/test.js
+++ gulpfile.js/task/test.js
@@ -3,30 +3,28 @@
 */
 module.exports = function (gulp, plugin, config) {
         
+    function runTest( done ) {
 
-<<<<<<< HEAD
-        function runTest( done ) {
+        var server = new plugin.karma.Server( {
+            configFile: __dirname.replace('gulpfile.js\\task', 'karma.conf.js'),
+            singleRun: true,
+            autoWatch: false
+        }, function( failCount) {
+            console.log( failCount );
+            if ( failCount ) {
+                plugin.notifier.notify( {
+                  title: 'Unit test failed',
+                  message: failCount + ' test' + (failCount > 1 ? 's' : '') + ' failed while running karma.\nCheck the command line',
+                  sound: false // Only Notification Center or Windows Toasters
+                } );
+            }
+            done();
+        } );
 
-            var server = new plugin.karma.Server( {
-                configFile: __dirname.replace('gulpfile.js\\task', 'karma.conf.js'),
-                singleRun: true,
-                autoWatch: false
-            }, function( failCount) {
-                console.log( failCount );
-                if ( failCount ) {
-                    plugin.notifier.notify( {
-                      title: 'Unit test failed',
-                      message: failCount + ' test' + (failCount > 1 ? 's' : '') + ' failed while running karma.\nCheck the command line',
-                      sound: false // Only Notification Center or Windows Toasters
-                    } );
-                }
-                done();
-            } );
+        server.start();
 
-            server.start();
-
-            return server;
-        }
+        return server;
+    }
 
     return function ( done ) {
         plugin.glob( config.glob.test , {}, function( err, files ) {
@@ -36,30 +34,5 @@ module.exports = function (gulp, plugin, config) {
                 done();
             }
         } )
-       
-=======
-    return function() {
-        plugin.karma.runner.run( {
-            configFile: config.client.path.karmaConfig,
-            singleRun: false 
-        }, function(exitCode) {
-            console.log('Karma has exited with ' + exitCode);
-            // process.exit(exitCode);
-            new Error('my error');
-        } );
-    	// var karmaRun = require('child_process').exec('karma run');
-
-    	// karmaRun.stdout.on('data', (data) => {
-    	// 	// if ( data.match('FAILED') ) {
-    	// 	// 	var lines = data.split('\n');
-
-    	// 	// 	for (var i = 0; i < lines.length; i++) {
-    	// 	// 		if ( lines[i].match('FAILED') ) {
-					// 	// plugin.notify();
-    	// 	// 		}
-    	// 	// 	};
-    	// 	// }
-    	// } );
->>>>>>> e455fc46c9645383dbb0508e4510ebda639992f2
     };
 };
